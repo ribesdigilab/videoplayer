4d7a64720c938f67d84d09b820cea8815454810e
Chiara Mastino <s309573@studenti.polito.it>
2025-06-13 11:39:55 +0200

    app iframe con logo

--- src/App.tsx
+++ src/App.tsx
@@ -23,55 +23,25 @@ const importAll = (r: ReturnType<WebpackRequire["context"]>) =>
     name: key.replace("./", "").replace(/\.[^/.]+$/, ""),
   }));
 
-const videos = importAll(requireWithContext.context("./videos", false, /\.(mp4)$/));
 
 export function App() {
-  const [currentVideo, setCurrentVideo] = useState<string | null>(null);
+
 
  const handleBack = () => {
-    setCurrentVideo(null);
+
   };
 
   return (
     <div className="app text-center">
-      <h1 className="text-2xl bg-blue-300 font-bold flex items-center py-8 pl-4">Video Jukebox</h1>
+      <h1 className="text-2xl bg-blue-300 font-bold flex items-center py-8 pl-4">Archivio Digitale</h1>
      {/* Se nessun video è selezionato, mostra la lista */}
-      {!currentVideo && (
-        <div className=" grid grid-cols-2 justify-center gap-4 px-16 pt-8">
-          {videos.map((video, index) => (
-            <button
-              key={index}
-              onClick={() => setCurrentVideo(video.src)}
-              className="bg-blue-700 text-white rounded hover:bg-gray-600 text-center text-sm transition "
-            >
-              <span className="break-words leading-tight">
-                {video.name}
-              </span>
-            </button>
-          ))}
-        </div>
-      )}
+        <iframe
+            src="http://www.ciseionline.it/2012/archivio.asp"
+            title="Ribes DigiLab"
+            className="w-screen h-screen border-none"
+            sandbox="allow-same-origin allow-scripts allow-popups allow-forms"
+        />
 
-
-      {currentVideo && (
-        <div className="mt-6">
-          <video
-            id="player"
-            controls
-            autoPlay
-            style={{ maxWidth: "90vw", maxHeight: "60vh" }}
-            src={currentVideo}
-          />
-          <div className="mt-4">
-            <button
-              onClick={handleBack}
-              className="bg-blue-700 text-white rounded hover:bg-gray-600 transition "
-            >
-              Indietro
-            </button>
-          </div>
-        </div>
-      )}
     </div>
   );
 }
